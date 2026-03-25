477a1d1e559bf6a89933149dd3eea1b4d5fd8933
Ayan-glitch-7 <mohdayan7245@gmail.com>
2026-03-25 15:04:09 +0000

    commit

---
+++ java script/15_object-3.js
@@ -0,0 +1,25 @@
+const course = {
+    courseName: "js",
+    coursePrice: 999,
+    courseLanguage: "hindi"
+}
+
+//course.courseLanguage
+
+const {courseLanguage: lang} = course;
+console.log(lang);     //hindi
+
+
+
+// {
+//     "name": "ayan",
+//     "course1": "js",
+//     "price": "free"
+// }
+
+
+// [
+//     {},
+//     {},
+//     {}
+// ]
