2389be206f8e77c35636e3213e7d7d66a1b84109
Ayan-glitch-7 <mohdayan7245@gmail.com>
2026-03-26 12:54:39 +0000

    c

---
+++ java script/23_control-flow-switch.js
@@ -0,0 +1,29 @@
+/*   SWITCH   */
+
+/*
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+*/
+
+const month = 3;
+
+switch (month) {
+    case 1:
+        console.log("january");   
+        break;
+    case 2:
+        console.log("febuary");  
+        break;
+    case 3:
+        console.log("march");  
+        break;
+    default:
+        console.log("invalid");  
+        break;
+}
