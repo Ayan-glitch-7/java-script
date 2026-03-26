db2f614d814d445aa7283210fd8dc7a7c50a9cee
Ayan-glitch-7 <mohdayan7245@gmail.com>
2026-03-26 10:49:47 +0000

    commit

---
+++ java script/18_scope.js
@@ -0,0 +1,18 @@
+let a = 10;
+const b = 20;
+var c = 30;
+
+console.log(a);        //10
+console.log(b);        //20
+console.log(c);        //30
+
+
+if (true) {
+    let a = 10;
+    const b = 20;
+    var c = 30;
+}
+
+// console.log(a);        //error
+// console.log(b);        //error
+console.log(c);        //30
