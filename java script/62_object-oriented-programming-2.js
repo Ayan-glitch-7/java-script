4906e6c1f699cce01acb95c7fafc036e89c8df54
Ayan-glitch-7 <mohdayan7245@gmail.com>
2026-04-15 16:26:35 +0000

    c

---
+++ java script/62_object-oriented-programming-2.js
@@ -0,0 +1,16 @@
+const user = {
+    username: "Ayan",
+    loginCount: 8,
+    signedIn: true,
+
+    getUserDetails: function() {
+        console.log("got user details from database");
+        console.log(`Username: ${this.username}`);
+        console.log(this);
+    }
+}
+
+console.log(user.username);
+console.log(user.getUserDetails());
+console.log(this);
+
